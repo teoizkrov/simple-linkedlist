ed636d29090c68d703e881cc58da39148c49148e
Teo <vinczetti@gmail.com>
2019-11-28 17:30:13 -0500

    fixed remove, added isempty.

--- hlinkedlist.h
+++ hlinkedlist.h
@@ -1,8 +1,26 @@
-struct LinkedList;
+struct Node
+{
+
+    int data;
+    struct Node *next;
+
+};
+struct LinkedList
+{
+
+    struct Node *head;
+    int cardinality;
+};
+
 void insert( struct LinkedList *, int );
 void removefromhead( struct LinkedList * );
 int retrieve( struct LinkedList *, int );
 int search( struct LinkedList *, int );
+void cleanup( struct LinkedList * );
+struct LinkedList new_LL();
+int isempty( struct LinkedList * );
+
+
 /*
  *Might add a function that returns a pointer to where it found an item, instead
  *of starting from the beginning if per se I wanted to search and remove
--- main.c
+++ main.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "hlinkedlist.h"
 
 int main( int argc, char* argv[] )
 {
 
+    struct LinkedList a = new_LL();
+    
+    insert( &a, 1 );
+    insert( &a, 2 );
+    insert( &a, 3 );
+
+    for( struct Node *p = a.head; p != NULL; p = p->next )
+        printf( "%d\n", p->data );
+
+    printf( "%d\n", a.cardinality );
+
+    cleanup( &a );
+    
+for( struct Node *p = a.head; p != NULL; p = p->next )
+    printf( "%d\n", p->data );
+
+
+printf( "%d\n", a.cardinality );
 
     return 0;
 }
 
-
-struct Node
-{
-
-    int data;
-    struct Node *next;
-
-};
-
-struct LinkedList
-{
-
-    struct Node *head;
-    int cardinality;
-
-};
 
 struct LinkedList new_LL()
 {
@@ -36,17 +39,14 @@ struct LinkedList new_LL()
 
 void insert( struct LinkedList *LL, int data )
 {
-
-    if( LL->head == NULL )
-        LL->head->data = data;
-    else
-    {
- 
-        struct Node *n = malloc( sizeof( struct Node ) );
-        n->data = data;
-        LL->head->next = n; 
     
-    }
+    struct Node *n = malloc( sizeof( struct Node ) );
+    n->data = data;
+
+    if( LL->head != NULL )
+        n->next = LL->head;
+
+    LL->head = n;
     LL->cardinality++;
 }
 
@@ -59,12 +59,13 @@ void removefromhead( struct LinkedList *LL )
         free( temp );
         LL->cardinality--;
     }
+    
 }
 
 int retrieve( struct LinkedList *LL, int index )
 {
     int c = 0; 
-    for( struct Node *p = LL->head; p != NULL; p++, c++ )
+    for( struct Node *p = LL->head; p != NULL; p = p->next, c++ )
         if( c==index )
            return p->data; 
     
@@ -74,7 +75,7 @@ int retrieve( struct LinkedList *LL, int index )
 int search( struct LinkedList *LL, int data )
 {
 
-    for( struct Node *p = LL->head; p != NULL; p++ )
+    for( struct Node *p = LL->head; p != NULL; p = p->next )
         if( p->data == data )
             return 1;
     
@@ -82,6 +83,20 @@ int search( struct LinkedList *LL, int data )
 
 }
 
+int isempty( struct LinkedList *LL )
+{
+    
+    return LL->cardinality == 0;
 
+}
+
+
+void cleanup( struct LinkedList *LL )
+{
+
+    while( !isempty( LL ) )
+        removefromhead( LL );
+    
+}
 
 
